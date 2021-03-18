c0334bc80683c347b6796a6734fb2f986ea55dfc
Gerald Buchholz <60468436+gbuchmsft@users.noreply.github.com>
2021-03-18 11:25:28 +0100

    Initial commit

---
+++ Automatic-ClientIP-NSG/provider.tf
@@ -0,0 +1,13 @@
+#Providers
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.43.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {
+  }
+}
